7794deb793bcd26520f42d87aad608668328c391
ersapatel <ersapatel@gmail.com>
2025-12-02 23:13:31 -0500

    Code changes for deployment and build

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
+  document.getElementById("root")
 );
